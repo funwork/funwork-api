7b5baaca29a01a6a59b2b92971d37a2c58b930fb
Changhwa Oh <changhwaoh.co@gmail.com>
2016-05-16 07:25:09 +0900

    init project

---
+++ src/main/java/io/funwork/api/FunworkApiApplication.java
@@ -0,0 +1,12 @@
+package io.funwork.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunworkApiApplication.class, args);
+	}
+}
